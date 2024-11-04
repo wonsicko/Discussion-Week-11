0559b992ec2a4ca3cb700207ee5082ad57f333e5
wonsicko <145046190+wonsicko@users.noreply.github.com>
2024-11-04 15:51:40 -0500

    Update csv_to_meets_html.py

--- csv_to_meets_html.py
+++ csv_to_meets_html.py
@@ -128,6 +128,7 @@ def csv_to_html(csv_filename, output_folder):
 
 
                      </footer>
+                     <script src="../js/imagePlaceholder.js"></script>
         </body>
 </html>
 """
